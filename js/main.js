f7c3516a5c3e1f2393b37d104a1eb82a18521c7d
Juan Felipe Ospina Jaramillo <juanfelipeospina127@gmail.com>
2022-10-05 19:20:00 -0500

    Footer Juan y Camila

--- js/main.js
+++ js/main.js
@@ -14,22 +14,20 @@ const participante5 = document.querySelector('.participante5');
 
 
 
-
+/* Acá empiezan los datos del participante 1 */
 const a = document.createElement('a');
 a.setAttribute('href', 'https://github.com/JuanfeOspiJara');
 participante1.append(a);
 a.innerText = 'Juan Felipe Ospina'
 
-/* const a = document.createElement('a'); */
-
-
 const img = document.createElement('img');
 img.setAttribute('src', 'imagenes/logogit.png');
 img.setAttribute('class','logo-git');
 participante1.append(img);
+/* Acá terminan los datos del participante 1*/
 
 
-
+/* Acá empiezan los datos del participante 2 */
 const a1 = document.createElement('a');
 a1.setAttribute('href', 'https://github.com/maria-betancourth');
 participante2.append(a1);
@@ -39,15 +37,16 @@ img1.setAttribute('src', 'imagenes/logogit.png');
 img1.setAttribute('class','logo-git');
 participante2.append(img1);
 a1.innerText = 'Maria Camila Ospina'
+/* Acá terminan los datos del participante 2 */
 
+/* Acá empiezan los datos del participante 3 */
 const a2 = document.createElement('a');
-a2.setAttribute('href', 'https://github.com/JuanfeOspiJara');
+a2.setAttribute('href', 'https://github.com/sevalenciagr');
 participante3.append(a2);
-/* const a = document.createElement('a'); */
-
 
 const img2 = document.createElement('img');
 img2.setAttribute('src', 'imagenes/logogit.png');
 img2.setAttribute('class','logo-git');
 participante3.append(img2);
 a2.innerText = 'Sebastian Valencia'
+/* Acá terminan los datos del participante 3 */
